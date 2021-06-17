5e45cc973ae6c0c237d537252e414d9350797991
mohammadhayajneh2014 <https://github.com/mohammadhayajneh2014>
2021-06-17 11:58:28 +0300

    lab10

--- modale/movie.js
+++ modale/movie.js
@@ -3,23 +3,39 @@ module.exports= movieHand;
 
 
 
+let ramStorMovie={};
+
+
 //localhost:3050/movie?moviecity=amman
 
 function movieHand(req, res) {
     let moviecity = req.query.moviecity;
     let key = process.env.movieApi;
-    let movieUrl =  `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${moviecity}&page=1`;
-    
 
-    axios.get(movieUrl).then(apiResult => {
-        const movieArray = apiResult.data.results.map(item => {
-            return new movieitem(item);
+    if (ramStorMovie[moviecity] !==undefined) {
+        //get data from my memory
+        res.send(ramStorMovie[moviecity]);
+        console.log('from memory');
+        
+    } else {
+        let movieUrl =  `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${moviecity}&page=1`;
+    
+        axios.get(movieUrl).then(apiResult => {
+            const movieArray = apiResult.data.results.map(item => {
+                return new movieitem(item);
+            })
+
+            ramStorMovie[moviecity]=movieArray;
+            console.log('from api');
+            res.send(movieArray);
         })
-        res.send(movieArray);
-    })
-        .catch(err => {
-            res.send(`there is an error in getting the data => ${err}`);
-        })
+            .catch(err => {
+                res.send(`there is an error in getting the data => ${err}`);
+            })
+        
+    }
+
+   
 }
 
 class movieitem {
